6ea50eac501c91e6d636fe007bcadebec47efb03
Naoki Sekiguchi <likealunatic@gmail.com>
2011-12-18 02:03:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# jQuery.scrollController
+
+This is a jQuery plugin that provides some scroll controlling.
+
+- highlighting scroll trigger/target elements
+- general smooth scrolling.
+
+## Required Framework
+
+- [jQuery](http://jquery.com/) (jQuery 1.7 or later)
+
+## Demos
+
+- http://likealunatic.jp/demo/jquery-scrollController/
+- http://likealunatic.jp/demo/jquery-scrollController/index_vt.html
+- http://likealunatic.jp/demo/jquery-scrollController/index_hr.html
+
+## Usage
+
+Load this script after jquery.js,  and call "scrollController" method when document is ready.
+
+	<script src="/js/jquery.js"></script>
+	<script src="/js/jquery.scrollController.js"></script>
+	<script>
+	$(function() {
+		$('a[href^="#"]').scrollController({
+			// some options...
+			horizontal: false
+		});
+	});
+	</script>
+
+Selector must be "A" elements that has hash anchor to specific element id.
+Attribute selector (like this:'a[href^="#"]') is convenient to select such "A" elements.
+
+## Options
+
+<table border="1">
+<colgroup span="1" class="colh">
+<colgroup span="1" class="colh">
+<colgroup span="1" class="cold">
+<thead>
+<tr>
+<th>Option name</th>
+<th>Default value</th>
+<th>Note</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>vertical</td>
+<td>true</td>
+<td>Whether controlling vertical scrolling or not. If you set this option to false, it'll be like this <a href="http://likealunatic.jp/demo/jquery-scrollController/index_hr.html">demo</a>.</td></td>
+</tr>
+<tr>
+<td>horizontal</td>
+<td>true</td>
+<td>Whether controlling horizontal scrolling or not. If you set this option to false, it'll be like this <a href="http://likealunatic.jp/demo/jquery-scrollController/index_vt.html">demo</a>.</td>
+</tr>
+<tr>
+<td>className</td>
+<td>"on"</td>
+<td>This will be added to the trigger/target elements to highlight.</td>
+</tr>
+<tr>
+<td>easing</td>
+<td>"swing"</td>
+<td>This will be passed to jQuery "<a href="http://api.jquery.com/animate/">animate()</a>" method option.</td>
+</tr>
+<tr>
+<td>duration</td>
+<td>750</td>
+<td>This will be passed to jQuery "<a href="http://api.jquery.com/animate/">animate()</a>" method option.</td>
+</tr>
+<tr>
+<td>delay</td>
+<td>500</td>
+<td>Delay of highlighting after scrolling. It's unit is millisecond.</td>
+</tr>
+</tbody>
+</table>
+
+## Notes
+
+Detection of highlighting depends on the center position of screen. When the position comes in the target area, the target/the connected trigger will be highlighted.
+
---
+++ jquery.scrollController.js
@@ -0,0 +1,156 @@
+/**
+ * $.scrollController
+ * smooth scrolling and highlight some element with scrolling
+ *
+ * @author     Naoki Sekiguchi (http://likealunatic.jp)
+ * @copyright  Naoki Sekiguchi (http://likealunatic.jp)
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    Release: 1.0
+ * @since      2011-12-17
+ */
+
+;(function($, window, document, undefined) {
+
+$.fn.scrollController = function (options) {
+	var opt = options || {};
+	opt.element = this;
+	this.data('scrollController', new ScrollController(opt));
+	return this;
+};
+function ScrollController() {
+	// extend default options
+	this.options = {};
+	$.extend(this.options, this.defaultOptions, arguments[0]);
+	this.element = arguments[0].element;
+	// to save target element
+	this.targets = [];
+	// initialize
+	this._create();
+}
+ScrollController.prototype = {
+	defaultOptions: {
+		element: {},
+		delay: 500,
+		className: 'on',
+		easing: 'swing',
+		duration: 750,
+		vertical: true,
+		horizontal: true
+	},
+	_create: function () {
+		var self = this;
+		var timer = {};
+		var options = this.options;
+		if (!options.vertical && !options.horizontal) { return; }
+
+		this.element.each(function (i, anchor) {
+			var target = self._getTarget(anchor);
+			if (!target) { return; }
+			// store each target data
+			$.data(anchor, 'target', target);
+			$.data(target, 'trigger', anchor);
+		});
+
+		this.element.on("click.scrollController", function(e) {
+			var target = $.data(this, 'target');
+			if (!target) { return; }
+			var x = Math.floor($(target).offset().left);
+			var y = Math.floor($(target).offset().top);
+			var style = {};
+			if (options.horizontal) { style.scrollLeft = x; }
+			if (options.vertical) { style.scrollTop = y; }
+			// to prevent highlight
+			$.data(window, 'preventHighlight', 1);
+			$('html, body').animate(style, {
+				easing: options.easing,
+				duration: options.duration,
+				complete: function () {
+					// clear preventing highlight with some delay
+					setTimeout(function () {
+						$.removeData(window, 'preventHighlight');
+					}, options.delay + 250);
+				}
+			});
+			// controll focus & blur
+			var clickedAnchor = this;
+			self.element.each(function (i, anchor) {
+				if (anchor === clickedAnchor) {
+					self._focus($.data(anchor, 'target'), anchor);
+				} else {
+					self._blur($.data(anchor, 'target'), anchor);
+				}
+			});
+			e.preventDefault();
+		});
+
+		$(window).on('scroll.scrollController', function () {
+			clearTimeout(timer);
+			timer = setTimeout(function () {
+				// if 'preventHighLight' flag is enabled, do nothing
+				if ($.data(window, 'preventHighlight')) { return; }
+				self._detectScroll();
+			}, options.delay);
+		});
+
+		// initialize
+		self._detectScroll();
+	},
+	_getTarget: function (linkElement) {
+		var href = $(linkElement).attr('href');
+		if (!href) { return false; }
+		var hash = href.slice(href.lastIndexOf('#'));
+		if (hash.charAt(0) != '#') {
+			// This link has no hash
+			return false;
+		}
+		if (hash) { return $(hash)[0]; }
+	},
+	_detectScroll: function () {
+		var self = this;
+		// get document scroll top position
+		var scrollX = document.documentElement.scrollLeft || document.body.scrollLeft;
+		var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+		// get window center position
+		var winCenterX = Math.floor($(window).width() / 2);
+		var winCenterY = Math.floor($(window).height() / 2);
+		// compute target position
+		var x = scrollX + winCenterX;
+		var y = scrollY + winCenterY;
+
+		this.element.each(function (i, trigger) {
+			var target = $.data(trigger, 'target');
+			if (!target) { return; }
+			var $target = $(target);
+			var judgeX, judgeY;
+			if (self.options.horizontal) {
+				judgeX = (x >= $target.offset().left &&
+					x < ($target.offset().left + $target.outerWidth()));
+			} else {
+				// if no "horizontal" option, go threw X-axis judge
+				judgeX = true;
+			}
+			if (self.options.vertical) {
+				judgeY = (y >= $target.offset().top &&
+					y < ($target.offset().top + $target.outerHeight()));
+			} else {
+				// if no "vertical" option, go threw Y-axis judge
+				judgeY = true;
+			}
+			if (judgeX && judgeY) {
+				self._focus(target, trigger);
+			} else {
+				self._blur(target, trigger)
+			}
+		});
+	},
+	_focus: function (target, trigger) {
+		$(target).addClass(this.options.className);
+		$(trigger).addClass(this.options.className);
+	},
+	_blur: function (target, trigger) {
+		$(target).removeClass(this.options.className);
+		$(trigger).removeClass(this.options.className);
+	}
+};
+
+})(jQuery, this, this.document);
